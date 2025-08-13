a8b7ceb80fca2c7c7e8495110b682de6dbc88ff1
kingvkay41-cyber <kingvkay41@gmail.com>
2025-08-13 13:39:00 +0530

    Add files via upload

---
+++ Task 1 (1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num= int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
+
+
+# In[ ]:
+
+
+
+
---
+++ Task 2 (1).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+a=0
+for i in range (1,51):
+    a=a+i
+print("The sum of number from 1 to 50 is :",a)
+
+
+# In[ ]:
+
+
+
+
